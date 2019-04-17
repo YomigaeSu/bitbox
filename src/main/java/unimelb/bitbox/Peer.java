1c59aed77ea6e2704e24ee110241e06cefa1df23
YomigaeSu <mn.yy@163.com>
2019-04-18 01:04:56 +1000

    Completed HANDSHAKE communication

--- src/main/java/unimelb/bitbox/Peer.java
+++ src/main/java/unimelb/bitbox/Peer.java
@@ -1,19 +1,13 @@
 package unimelb.bitbox;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.security.NoSuchAlgorithmException;
-import java.util.Scanner;
 import java.util.logging.Logger;
 
 import org.json.simple.JSONObject;
@@ -26,6 +20,8 @@ public class Peer {
 	private static Logger log = Logger.getLogger(Peer.class.getName());
 
 	// Hard coded the ip addresses and ports for test purpose
+	// Peer1 settings: port 8111; peerPort 8112
+	// Peer2 settings: port 8112; peerPort 8113
 	private static String ip = "localhost";
 	private static int port = 8111;
 	private static String peerIp = "localhost";
@@ -39,21 +35,22 @@ public class Peer {
 		ServerMain ser = new ServerMain();
 
 		Thread t1 = new Thread(new Runnable() {
+			@Override
 			public void run() {
 
 				Socket socket = null;
 				try {
 					// Create a stream socket bounded to any port and connect it to the
 					// socket bound to localhost on port 8111
-					socket = new Socket(ip, port);
-					System.out.println("Connection established");
+					socket = new Socket(peerIp, peerPort);
+					System.out.println("Working as clientPeer: Connection established");
 
 					// Get the input/output streams for reading/writing data from/to the socket
 					DataInputStream in = new DataInputStream(socket.getInputStream());
 					DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 
-					out.writeUTF("Peer hosting on" + ip + ":" + port);
-					out.flush();
+//					out.writeUTF("Peer hosting on" + ip + ":" + port);
+//					out.flush();
 
 					// Send a HANDSHAKE_REQUEST to another peer by writing to the socket output
 					// stream
@@ -65,20 +62,35 @@ public class Peer {
 					// Write {"hostPort":{"host","localhost","port","8118"}} into request
 					request.put("hostPort", hostPort);
 
-					System.out.println("Handshake Request sent: " + request.toJSONString());
+					out.writeUTF(request.toJSONString());
+					out.flush();
+					System.out.println("COMMAND SENT: " + request.toJSONString());
 
-					// Waiting for the Handshake Response
+					// Waiting for the server peer's response
 					while (true) {
 
-						// Receive the reply from the server by reading from the socket input stream
-						String received = in.readUTF();
-						System.out.println("Message received: " + received);
+						// Receive the reply from the server peer by reading from the socket input
+						// stream
+						if (in.available() > 0) {
+
+							String received = in.readUTF();
+							System.out.println("COMMAND RECEIVED: " + received);
+
+							// Marshal the result into JSONObject
+							JSONParser parser = new JSONParser();
+							JSONObject message = (JSONObject) parser.parse(received);
+
+						}
+
 					}
 
 				} catch (UnknownHostException e) {
 					e.printStackTrace();
 				} catch (IOException e) {
 					// e.printStackTrace();
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				} finally {
 					// Close the socket
 					if (socket != null) {
@@ -93,31 +105,31 @@ public class Peer {
 		});
 
 		Thread t2 = new Thread(new Runnable() {
+			@Override
 			public void run() {
 
-				
 				ServerSocket listeningSocket = null;
 				Socket clientSocket = null;
 
 				try {
 					// Create a server socket listening on port 8112
-					listeningSocket = new ServerSocket(peerPort);
+					listeningSocket = new ServerSocket(port);
 					int i = 0; // counter to keep track of the number of clients
 
-					SocketAddress add = listeningSocket.getLocalSocketAddress();
+//					SocketAddress add = listeningSocket.getLocalSocketAddress();
 
 					// Listen for incoming connections for ever
 					while (true) {
-						System.out.println("Server listening on port" + add + " for a connection");
+						System.out.println("Server listening on" + ip + ":" + port + " for a connection");
 
 						// Accept an incoming client connection request
 						clientSocket = listeningSocket.accept(); // This method will block until a connection request is
 																	// received
 						i++;
-						System.out.println("Client conection number " + i + " accepted:");
-						System.out.println("Remote Port: " + clientSocket.getPort());
-						System.out.println("Remote Hostname: " + clientSocket.getInetAddress().getHostName());
-						System.out.println("Local Port: " + clientSocket.getLocalPort());
+						System.out.println("Working as serverPeer: Client conection number " + i + " accepted:");
+//						System.out.println("Remote Port: " + clientSocket.getPort());
+//						System.out.println("Remote Hostname: " + clientSocket.getInetAddress().getHostName());
+//						System.out.println("Local Port: " + clientSocket.getLocalPort());
 
 						// Get the input/output streams for reading/writing data from/to the socket
 						DataInputStream in = new DataInputStream(clientSocket.getInputStream());
@@ -129,7 +141,7 @@ public class Peer {
 						// code of this loop is executed, incoming connections have to wait until the
 						// current
 						// one is processed unless...we use threads!
-                        // String clientMsg = null;
+						// String clientMsg = null;
 						try {
 							// The JSON Parser
 							JSONParser parser = new JSONParser();
@@ -138,10 +150,16 @@ public class Peer {
 							while (true) {
 								if (in.available() > 0) {
 									// Attempt to convert read data to JSON
-									JSONObject command = (JSONObject) parser.parse(in.readUTF());
-									System.out.println("COMMAND RECEIVED: " + command.toJSONString());
-									// Generate a response based on the request
-									parseMessage(command, out);
+									String received = in.readUTF();
+									System.out.println("COMMAND RECEIVED: " + received);
+									JSONObject command = (JSONObject) parser.parse(received);
+
+									// Generate a HANDSHAKE_RESPONSE
+									String message = handleHandshake(command);
+									out.writeUTF(message);
+									out.flush();
+									System.out.println("COMMAND SENT: " + message);
+
 								}
 
 							}
@@ -182,12 +200,17 @@ public class Peer {
 	 * @param message The massage received
 	 * @param out     Data Output Stream bound to the socket
 	 */
-	private static void parseMessage(JSONObject message, DataOutputStream out) {
+	private static String handleHandshake(JSONObject message) {
 		// TODO The method should also check if the Handshake has been completed
 		// If yes, send a INVALID_PROTOCOL
+
+		// A placeholder for the outgoing command message.
 		JSONObject response = new JSONObject();
+
 		if (message.get("command").equals("HANDSHAKE_REQUEST")) {
+
 			// The message received is a Handshake request
+			// Generate a Handshake response JSONO
 			response.put("command", "HANDSHAKE_RESPONSE");
 			JSONObject hostPort = new JSONObject();
 			hostPort.put("host", ip);
@@ -199,13 +222,7 @@ public class Peer {
 			response.put("command", "INVALID_PROTOCOL");
 			response.put("message", "message must contain a command field as string");
 		}
-		try {
-			// Marshal response and send it to the sender
-			out.writeUTF(response.toJSONString());
-			out.flush();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		return response.toJSONString();
 	}
 
 	public void connect() {
